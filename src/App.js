8679ff1a84584a422c7b7147bfaa69885405e047
lewis_ccx <lewis.cao.ext@razer.com>
2022-07-16 15:59:00 +0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          {`Hello ${process.env.REACT_APP_NAME} cao`}
         </a>
       </header>
     </div>
